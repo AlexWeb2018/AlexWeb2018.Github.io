97807c505e3926a72161a5bdb5b22c57e1d4b0be
AlexWeb2018 <38156259+AlexWeb2018@users.noreply.github.com>
2018-06-21 18:02:58 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+$(document).ready(function (){
+
+//Массив ответов
+var answers = {
+    'привет': 'И тебе привет!',
+    'сколько тебе лет': '21',
+    'кто ты': 'Я Робот!',
+    'как дела': 'У меня отлично! А у тебя я даже спрашивать не буду!',
+    'как тебя зовут': 'Konnor version 0.0.0.15.3',
+    'кто тебя создал': 'создатель',
+    'что ты умеешь?' : 'всякую херню'
+};
+ 
+//Флаг того, что найден ответ на вопрос
+var have_answer = false; 
+ 
+$('#send').on("submit", function(){
+     
+    var text = $('#text').val();
+     
+    if( text != '' ){
+         
+        for (var i in answers){
+             
+            if( i == text.toLowerCase().replace(/[/.,!?;]*/g, '').trim() ){
+                $('result').append('<p>' + answers[i] + '</p>');
+                have_answer = true;
+                break;
+            }
+        }
+        if( !have_answer ) $('result').append('<p>говори нормально сука!</p>');
+        var have_answer = false;
+        $('#text').val('').focus();
+         
+    }else $('result').append('<p>Не нужно пустых слов..</p>');
+ 
+});
+});
+
+
+
